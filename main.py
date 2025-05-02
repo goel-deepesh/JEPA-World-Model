b39cc321d07bc2c6a57c88c8c9b761f6311d86c7
Deepesh Goel <deepeshgoel20@gmail.com>
2025-05-02 08:46:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def load_model():
     # TODO: Replace MockModel with your trained model
     device = get_device()
     model = MainModel()
-    model.load_state_dict(torch.load("checkpoint/best_model.pth", weights_only=True))
+    model.load_state_dict(torch.load("continued_training/jepa_model_epoch_final.pth", weights_only=True))
     model.to(device)
     return model
 
